9a9d3d698a573a601dd9aeea6d09315f0100f11b
zhuvoy <144527693+zhuvoy@users.noreply.github.com>
2025-05-11 14:08:10 +0200

    version 1.2.0

--- src/civ_version.h
+++ src/civ_version.h
@@ -17,10 +17,10 @@
 #ifndef CIV_VERSION_H
 #define CIV_VERSION_H
 
-#define CIV_VERSION		"1.1.3"
+#define CIV_VERSION		"1.2.0"
 #define CIV_MAJOR_VERSION	(1)
-#define CIV_MINOR_VERSION	(1)
-#define CIV_MICRO_VERSION	(3)
+#define CIV_MINOR_VERSION	(2)
+#define CIV_MICRO_VERSION	(0)
 
 
 #endif //CIV_VERSION_H
